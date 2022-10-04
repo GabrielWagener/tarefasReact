4e582b9f3c7ab6945a987962cfc7f0ddd94e6ae1
GabrielWagener <gabrieleduardowagener@gmail.com>
2022-10-04 20:54:28 -0300

    Pronto

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [task, setTask] = useState("");
   const [listTask, setListTask] = useState([]);
 
+  //Metódo para adicionar tarefa
   const addTask = () => {
     if (!task) return alert("Preencha uma tarefa");
     const newTask = {
@@ -15,25 +16,51 @@ function App() {
     };
     setListTask([...listTask, newTask]);
     setTask("");
+
   };
 
+  //Método para remover tarefa
   const removeTask = (id) => {
     const newList = listTask.filter((task) => task.id !== id);
     setListTask(newList);
   };
 
+  //Método para marcar tarefa como concluída
   const toggleChecked = (id, checked) => {
     const index = listTask.findIndex((task) => task.id === id);
     const newList = listTask;
     newList[index].checked = !checked;
     setListTask([...newList]);
   };
+  
+  //Método para obter quantidade total de tarefas
+  const counterTotalTask = () => {
+    return listTask.length
+  };
 
-  addTask.length;
+  //Método para obter quantidade total de tarefas concluídas
+  const counterCheckedTask = () => {
+   const task = listTask.filter((task) => task.checked == true)
+   return task.length
+  
+  };
 
+  //Metódo para obter quantidade total de tarefas não concluídas
+  const counterNotCheckedTask = () => {
+    const task = listTask.filter((task) => task.checked == false)
+    return task.length
+   
+   };
+  
   return (
+    
     <Container>
       <h1 className="title">Lista de Tarefas</h1>
+      <h2 className="title-Quantidades">Quantidade:{counterTotalTask()}</h2>
+      <h2 className="title2-Concluidas">Concluídas:{counterCheckedTask()}</h2>
+      <h2 className="title2-naoconcluidas">Não Concluídas:{counterNotCheckedTask()}</h2>
+
+
       <Spacer />
 
       <Flex direction="row">
@@ -53,10 +80,10 @@ function App() {
               <p>{task.task}</p>
               <Flex direction="row">
                 <button onClick={() => toggleChecked(task.id, task.checked)}>
-                  <i class="bx bx-check "></i>
+                  <i className="bx bx-check "></i>
                 </button>
                 <button onClick={() => removeTask(task.id)}>
-                  <i class="bx bx-trash "></i>
+                  <i className="bx bx-trash "></i>
                 </button>
               </Flex>
             </Item>
